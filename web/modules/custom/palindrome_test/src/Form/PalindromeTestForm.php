e3af3ca9d8498a5e60d1c904e4e2541aa8e26001
Kimble <kimble@vardaman.com>
2020-01-16 08:49:53 -0600

    Improving message with Messenger service, added emoji, forced uppercase.

--- web/modules/custom/palindrome_test/src/Form/PalindromeTestForm.php
+++ web/modules/custom/palindrome_test/src/Form/PalindromeTestForm.php
@@ -43,13 +43,7 @@ class PalindromeTestForm extends FormBase {
   public function validateForm(array &$form, FormStateInterface $form_state) {
     
     $word = $form_state->getValue('word');
-    // Check if value is equal to its reverse
-    if (strrev($word) == $word) {   
-      $form_state->setErrorByName('word', $this->t('This is a palindrome!'));   
-    } 
-    else { 
-      $form_state->setErrorByName('word', $this->t('Sorry, this is not palindrome!')); 
-    } 
+
     parent::validateForm($form, $form_state);
   }
 
@@ -58,9 +52,22 @@ class PalindromeTestForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Display result.
-    foreach ($form_state->getValues() as $key => $value) {
+    /*foreach ($form_state->getValues() as $key => $value) {
       \Drupal::messenger()->addMessage($key . ': ' . ($key === 'text_format'?$value['value']:$value));
+    }*/
+    $word = strtoupper($form_state->getValue('word'));
+    // Check if value is equal to its reverse
+    // and assign the proper message.
+    if (strrev($word) == $word) {
+      $prefix = "(-: ";
+      $msg = $this->t(' is a palindrome!');   
+    } 
+    else { 
+      $prefix = ")-: ";
+      $msg = $this->t(' is not palindrome!'); 
     }
+    // Print result
+    \Drupal::messenger()->addMessage($prefix . $word . $msg);
   }
 
 }
