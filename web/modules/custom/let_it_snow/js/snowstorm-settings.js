f324647b1a86cb7914a30dd6afb7cb0d990f9fdc
Kimble <kimble@vardaman.com>
2020-01-25 12:58:29 -0600

    Storing work in progress.

---
+++ web/modules/custom/let_it_snow/js/snowstorm-settings.js
@@ -0,0 +1,5 @@
+(function ($, Drupal, drupalSettings) {
+  return  "snowStorm.snowColor = '#c00';";
+}(jQuery, Drupal, drupalSettings));
+// Snow is getting red from the below statement, but not the above???
+snowStorm.snowColor = '#c00';
